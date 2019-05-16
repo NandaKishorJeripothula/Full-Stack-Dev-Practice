971e7b3b71f36db7060387809c74b92b1d4c6d3e
Nandu <j.kishor.bd@gmail.com>
2019-05-16 13:16:26 +0530

    autosuggest component separated

--- posts-application-front-end/src/components/Home.component.js
+++ posts-application-front-end/src/components/Home.component.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import PostListViewCard from './PostListViewCard.component';
-import Autosuggest from 'react-autosuggest';
-import { Link } from 'react-router-dom';
+import SearchUser from './SearchUser.component';
 export default class Home extends Component {
     constructor(props) {
         super(props);
@@ -11,52 +10,9 @@ export default class Home extends Component {
             errorMessage: null,
             postsLimit: 10,
             postsData: [],
-            usersData: [],
-            value: '',
-            suggestions: []
-
         }
     }
-
-    //Methods Related to the AutoSuggest Content
-    onChange = (event, { newValue, method }) => {
-        this.setState({
-            value: newValue
-        });
-    };
-    onSuggestionsFetchRequested = ({ value }) => {
-        this.setState({
-            suggestions: getSuggestions(value, this.state.usersData)
-        });
-    };
-    onSuggestionsClearRequested = () => {
-        this.setState({
-            suggestions: []
-        });
-    };
     //API handlers
-    fetchUsers = async () => {
-        var uri = "http://jsonplaceholder.typicode.com/users";
-        var usersData = await axios(
-            uri,
-            {
-                method: 'GET',
-                "headers": { "accept": "text/html,application/xhtml+xml,application/xml" },
-            }
-        ).then((resp) => {
-            if (resp.status === 200) {
-                return (resp.data.length === 0) ? false : resp.data;
-            } else {
-                this.setState({ errorMessage: resp.statusMessage });
-                return false;
-            }
-        }).catch((err) => {
-            this.setState({ errorMessage: err });
-            return false;
-        });
-        this.setState({ usersData: this.state.usersData.concat(usersData) });
-        console.log(usersData);
-    }
     fetchPosts = async () => {
         var uri = `http://jsonplaceholder.typicode.com/posts?_start=${this.state.lastPostId}&_limit=${this.state.postsLimit}`
         var postsData = await axios(
@@ -80,17 +36,12 @@ export default class Home extends Component {
         this.setState({ postsData: this.state.postsData.concat(postsData) })
         // console.log("updated states\n", this.state.postsData)
     }
+
     componentDidMount() {
         this.fetchPosts();
-        this.fetchUsers();
     }
+
     render() {
-        const inputProps = {
-            placeholder: 'Type a username',
-            value: this.state.value,
-            onChange: this.onChange
-        };
-        const { suggestions } = this.state;
         let postsLoader;
         if (this.state.postsData.length !== 0) {
             postsLoader = this.state.postsData.map((element, i) => (
@@ -99,15 +50,7 @@ export default class Home extends Component {
         }
         return (
             <div>
-                <h2>Search a User</h2>
-                <Autosuggest
-                    suggestions={suggestions}
-                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-                    getSuggestionValue={getSuggestionValue}
-                    renderSuggestion={renderSuggestion}
-                    inputProps={inputProps}
-                />
+                <SearchUser />
                 <h5>{this.state.errorMessage}</h5>
                 <h3>Posts</h3>
                 {postsLoader}
@@ -115,21 +58,3 @@ export default class Home extends Component {
         )
     }
 }
-const renderSuggestion = suggestion => (
-    <div>
-        <Link to={{ pathname: '/userView', state: [suggestion] }} >
-            {suggestion.username}
-            {console.log("From Rdenr" + suggestion)}
-        </Link>
-    </div>
-);
-const getSuggestionValue = suggestion => suggestion.username;
-
-const getSuggestions = (value, usersData) => {
-    const inputValue = value.trim().toLowerCase();
-    const inputLength = inputValue.length;
-
-    return inputLength === 0 ? [] : usersData.filter(user =>
-        user.username.toLowerCase().slice(0, inputLength) === inputValue
-    );
-};
---
+++ posts-application-front-end/src/components/SearchUser.component.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import Autosuggest from 'react-autosuggest';
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+export default class SearchUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            usersData: [],
+            value: '',
+            suggestions: []
+        }
+    }
+    fetchUsers = async () => {
+        var uri = "http://jsonplaceholder.typicode.com/users";
+        var usersData = await axios(
+            uri,
+            {
+                method: 'GET',
+                "headers": { "accept": "text/html,application/xhtml+xml,application/xml" },
+            }
+        ).then((resp) => {
+            if (resp.status === 200) {
+                return (resp.data.length === 0) ? false : resp.data;
+            } else {
+                this.setState({ errorMessage: resp.statusMessage });
+                return false;
+            }
+        }).catch((err) => {
+            this.setState({ errorMessage: err });
+            return false;
+        });
+        this.setState({ usersData: this.state.usersData.concat(usersData) });
+        console.log(usersData);
+    }
+    //Methods Related to the AutoSuggest Content
+    onChange = (event, { newValue, method }) => {
+        this.setState({
+            value: newValue
+        });
+    };
+    onSuggestionsFetchRequested = ({ value }) => {
+        this.setState({
+            suggestions: getSuggestions(value, this.state.usersData)
+        });
+    };
+    onSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: []
+        });
+    };
+    componentDidMount() {
+        this.fetchUsers();
+    }
+    render() {
+        const inputProps = {
+            placeholder: 'Type a username',
+            value: this.state.value,
+            onChange: this.onChange
+        };
+        const { suggestions } = this.state;
+        return (
+            <div>
+                <h4>Search a User</h4>
+                <Autosuggest
+                    suggestions={suggestions}
+                    onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                    onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                    getSuggestionValue={getSuggestionValue}
+                    renderSuggestion={renderSuggestion}
+                    inputProps={inputProps}
+                />
+            </div>
+        )
+    }
+}
+const getSuggestionValue = suggestion => suggestion.username;
+const getSuggestions = (value, usersData) => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+    return inputLength === 0 ? [] : usersData.filter(user =>
+        user.username.toLowerCase().slice(0, inputLength) === inputValue
+    );
+};
+const renderSuggestion = suggestion => (
+    <div>
+        <Link to={{ pathname: '/userView', state: [suggestion] }} >
+            {suggestion.username}
+        </Link>
+    </div>
+);
